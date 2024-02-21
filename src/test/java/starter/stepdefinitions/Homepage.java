227a7dfe8e375551c3e2cd556c32344bb253f8f5
DishaOwitechs <132178941+DishaOwitechs@users.noreply.github.com>
2024-02-21 13:28:34 +1100

    Update with new Testcase

--- src/test/java/starter/stepdefinitions/Homepage.java
+++ src/test/java/starter/stepdefinitions/Homepage.java
@@ -1,11 +1,8 @@
 package starter.stepdefinitions;
 
-import io.cucumber.java.en.And;
-import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
